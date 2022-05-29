0484c1167da6f0200266b184d31431ffe792b0ae
Federico <macbookpro@MacBooks-MacBook-Pro.local>
2022-05-29 18:12:02 -0300

    add claimable messages to eth_proxy canister

--- eth_bridge/ic/src/eth_proxy/src/common/types.rs
+++ eth_bridge/ic/src/eth_proxy/src/common/types.rs
@@ -12,12 +12,21 @@ pub type MessageHash = String;
 
 pub type EthereumAddr = Principal;
 
-pub type TxReceipt = Result<Nat, TxError>;
+pub type MsgHashKey = [u8; 32];
 
-pub type MagicResponse = Result<Principal, FactoryError>;
+pub type TxReceipt = Result<Nat, TxError>;
 
 #[derive(Serialize, CandidType, Deserialize)]
 pub struct Message;
+
+#[derive(CandidType, Deserialize, Clone, Debug)]
+pub struct ClaimableMessage {
+    pub owner: EthereumAddr,
+    pub msg_key: MsgHashKey,
+    pub msg_hash: MessageHash,
+    pub token: TokendId,
+    pub amount: Nat,
+}
 
 #[derive(CandidType, Deserialize, Debug)]
 pub enum FactoryError {
@@ -63,6 +72,8 @@ pub struct ProxyState {
     pub balances: RefCell<HashMap<Principal, HashMap<TokendId, Nat>>>,
     /// authorized principals
     pub controllers: RefCell<Vec<Principal>>,
+    // store outgoing massages waiting to be claimed
+    pub messages_unclaimed: RefCell<HashMap<EthereumAddr, Vec<ClaimableMessage>>>,
 }
 
 #[derive(CandidType, Deserialize, Default)]
@@ -73,6 +84,8 @@ pub struct StableProxyState {
     pub balances: HashMap<Principal, HashMap<Principal, Nat>>,
     /// authorized principals
     pub controllers: Vec<Principal>,
+    // store outgoing massages waiting to be claimed
+    pub messages_unclaimed: HashMap<EthereumAddr, Vec<ClaimableMessage>>,
 }
 
 #[derive(CandidType, Deserialize, Clone, Copy)]
